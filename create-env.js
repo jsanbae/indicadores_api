58e21f33351b9a93d06ba8d491d6e5181d56c2d1
Javier Sanchez <jsan5709@hotmail.com>
2022-08-08 16:50:31 -0400

    build: create-env.js script

---
+++ create-env.js
@@ -0,0 +1,9 @@
+// Create .env for Netflify
+const fs = require('fs')
+fs.writeFileSync('./.env', `PORT=${process.env.PORT}\n`)
+fs.writeFileSync('./.env', `LOGGER_LEVEL=${process.env.LOGGER_LEVEL}\n`)
+fs.writeFileSync('./.env', `LOGGER_ENABLED=${process.env.LOGGER_ENABLED}\n`)
+
+fs.writeFileSync('./.env', `DATABASE=${process.env.DATABASE}\n`)
+fs.writeFileSync('./.env', `DATABASE_LOCAL=${process.env.DATABASE_LOCAL}\n`)
+fs.writeFileSync('./.env', `DATABASE_PASSWORD=${process.env.DATABASE_PASSWORD}\n`)
