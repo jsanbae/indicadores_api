8c83c3a7900b464948e6a98f80f4ace6ab843a45
Javier Sanchez <jsan5709@hotmail.com>
2022-08-10 10:35:14 -0400

    feat: api expose utm indicador

--- src/indicadores/index.js
+++ src/indicadores/index.js
@@ -2,10 +2,12 @@
 
 const UFIndicador = require('./uf')
 const IVPIndicador = require('./ivp')
+const UTMIndicador = require('./utm')
 
 const api = {
   uf: UFIndicador,
-  ivp: IVPIndicador
+  ivp: IVPIndicador,
+  utm: UTMIndicador
 }
 
 module.exports = api
