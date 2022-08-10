5bf6a767c0b685a3650a3815a6155a0bd3acfb86
Javier Sanchez <jsan5709@hotmail.com>
2022-08-10 10:48:00 -0400

    delete serverless config

--- functions/server-less.js
+++
@@ -1,6 +0,0 @@
-/* example using https://github.com/dougmoscrop/serverless-http */
-const serverless = require('serverless-http')
-const app = require('../web/app')
-
-// Export lambda handler
-exports.handler = serverless(app)
