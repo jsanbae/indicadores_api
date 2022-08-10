853f90ac0b097f9c690862680f8a4f98663949b9
Javier Sanchez <jsan5709@hotmail.com>
2022-08-10 10:24:32 -0400

    feat: new fetch functions

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -1,19 +1,31 @@
-const fetch = require('node-fetch')
+const nodeFetch = require('node-fetch')
 const { fetchBuilder, MemoryCache } = require('node-fetch-cache');
 const cache_time = 1000*60*60*24// 1 day
 const fetchcache = fetchBuilder.withCache(new MemoryCache({ttl:cache_time}));
 
-const fetchCacheData = async (url) => {
+const fetch = async (url) => {
+  const response = await nodeFetch(url);
+  return response;
+}
+
+const fetchBody = async (url) => {
+  const response = await nodeFetch(url);
+  return response.text();
+}
+
+const fetchCache = async (url) => {
+  const response = await fetchcache(url);
+  return response;
+}
+
+const fetchCacheBody = async (url) => {
   const response = await fetchcache(url);
   return response.text();
 }
 
-const fetchData = async (url) => {
-    const response = await fetch(url);
-    return response.text();
-}
-
 module.exports = {
-    fetchCacheData: fetchCacheData,
-    fetchData: fetchData
+  fetch: fetch,
+  fetchBody: fetchBody,   
+  fetchCache: fetchCache,
+  fetchCacheBody: fetchCacheBody
 }
